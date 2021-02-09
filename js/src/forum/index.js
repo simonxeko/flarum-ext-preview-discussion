a647f2f337fe0a502d6e0fc2c0d4e399d50c8ffe
iPurpl3x <rafael.horvat@icloud.com>
2021-02-09 10:00:10 +0100

    Huge refactoring. Updated for beta.15.

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,61 +1,95 @@
-import TextEditor from 'flarum/components/TextEditor';
+/*
+ * This file is part of simonxeko/preview-discussion.
+ *
+ * Copyright (c) 2021 simonxeko & iPurpl3x.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+import app from 'flarum/app';
+import { extend } from 'flarum/extend';
+import ComposerState from 'flarum/states/ComposerState';
 import ComposerBody from 'flarum/components/ComposerBody';
 import DiscussionComposer from 'flarum/components/DiscussionComposer';
-import { extend } from 'flarum/extend';
+import TextEditor from 'flarum/components/TextEditor';
 
 app.initializers.add('simonxeko/preview-discussion', () => {
-  console.log('[simonxeko/preview-discussion] Hello, forum!');
+	// For the `DiscussionComposer` add a property to the `ComposerState` used to
+	// check if the preview is being whown or not. The will be accessible globally by
+	// accessing `app.composer.showPreview`.
+	extend(ComposerState.prototype, 'load', function (_, componentClass) {
+		if (componentClass === DiscussionComposer) {
+			this.showPreview = false;
+		}
+	});
 
-  let index = 1;
-  let textareaId = 'textarea1';
-  let previewMode = false;
-  
+	// Add an empty element to the `ComposerBody.headerItems`. This will be filled with content
+	// once the preview is shown.
+	extend(ComposerBody.prototype, 'headerItems', function (items) {
+		items.add(
+			'preview-discussion',
+			<div
+				className={`Composer-preview Post-body ${app.composer.showPreview ? '' : 'hidden'}`}
+			></div>,
+			50
+		);
+	});
 
-  let onClickPreview = () => { 
-    previewMode = !previewMode;
-    if (previewMode) {
-      s9e.TextFormatter.preview($('#' + textareaId).val(), $('#preview-discussion')[0]);
-      $('#preview-discussion').show();
-    } else {
-      $('#preview-discussion').hide();
-    }
-  }
+	// Add a `jumpToPreview` method to the `DiscussionComposer`. Doing that implies
+	// that the button for preview will be shown in the `controlItems` of the `TextEditor`.
+	// Thiw method will be the callback called when this button is clicked.
+	DiscussionComposer.prototype.jumpToPreview = function () {
+		// Update the state
+		this.composer.showPreview = !this.composer.showPreview;
 
-  extend(DiscussionComposer.prototype, 'init', function() {
-    this.editor.props.preview = () => {
-      onClickPreview();
-    }
-  });
+		// If we need to show the preview...
+		if (this.composer.showPreview) {
+			// ... Set the content of the dedicated element using the `s9e.TextFormatter`
+			s9e.TextFormatter.preview(
+				this.composer.fields.content(),
+				this.$('.Composer-preview')[0]
+			);
+		}
+	};
 
-  extend(TextEditor.prototype, 'init', function() {
-    textareaId = 'textarea'+(index++);
-  });
+	extend(TextEditor.prototype, 'view', function (vdom) {
+		// When showing the preview we need to alter the UI:
+		if (app.composer.showPreview) {
+			// Hide the textarea
+			try {
+				if (vdom.children[0].tag === 'textarea') {
+					vdom.children[0].attrs.className += ' hidden';
+				}
+			} catch {}
 
-  extend(TextEditor.prototype, 'view', function(vdom) {
-    if (!vdom.children[0].attrs.id) { // Check id to avoid conflicts with markdown extension
-      vdom.children[0].attrs.id = this.textareaId;
-    } 
-  });
+			// Make the preview button look active
+			try {
+				const itemPreview = vdom.children[1].children[0].children.find(
+					(item) => item.attrs.className === 'item-preview'
+				);
+				if (itemPreview) {
+					itemPreview.children[0].attrs.className += ' active';
+				}
+			} catch {}
+		}
+	});
 
-  extend(TextEditor.prototype, 'oninput', function() {
-    // Simultaneously render preview
-    // s9e.TextFormatter.preview($('#' + textareaId).val(), $('#preview-discussion')[0]);
-  });
-
-  extend(ComposerBody.prototype, 'headerItems', function(items) {
-    items.add('preview-discussion', 
-      <div id="preview-discussion" class="Post-body" style="display: none; position: absolute; background: white; z-index: 99;">TEST_PREVIEW</div>,
-    50);
-  });
-
-  setInterval(() => {
-    if ($('#' + textareaId).offset()) {
-      $('#preview-discussion').css({
-        width: $('#' + textareaId).width(),
-        height: $('#' + textareaId).height() + 10,
-        top: $('#' + textareaId).offset().top - $('.Composer').offset().top,
-        left: $('#' + textareaId).offset().left - $('.Composer').offset().left,
-      });
-    }
-  }, 300);
+	// To make that the preview container has the right size and position (including when the composer
+	// is resized) we need to continually update it.
+	extend(ComposerBody.prototype, 'oncreate', function () {
+		setInterval(function () {
+			if (app.composer.showPreview) {
+				const $textarea = this.$('.TextEditor textarea');
+				if ($textarea.offset()) {
+					this.$('.Composer-preview').css({
+						width: $textarea.width(),
+						height: $textarea.height() + 10,
+						top: $textarea.offset().top - $('.Composer').offset().top,
+						left: $textarea.offset().left - $('.Composer').offset().left,
+					});
+				}
+			}
+		}, 100);
+	});
 });
